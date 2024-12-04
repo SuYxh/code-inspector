97aa64da4f7e209bc6b85dc62452a14bfbb0202f
yangxinhao <yangxinhao@zhuanzhuan.com>
2024-12-04 23:07:00 +0800

    feat: 开启调试

--- packages/webpack-plugin/vite.inject-loader.config.ts
+++ packages/webpack-plugin/vite.inject-loader.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: ['src/inject-loader.ts'],
       formats: ['cjs'],
